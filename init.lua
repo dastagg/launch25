716ad663d1f625cde5e6edf2b42172b1964fae76
dastagg:opus8 <dastagg@stagglinux.com>
2024-12-14 10:49:36 -0500

    Add colorizer.

--- init.lua
+++ init.lua
@@ -29,6 +29,7 @@ spec("user.extras.illuminate") -- highlight all other words under cursor
 spec("user.extras.autopairs") -- auto create the other half of the pair of {}, (), []
 spec("user.extras.neotab") -- tab in and out of {}, (), [] and more
 spec("user.extras.nvim-surround") -- Add/delete/change surrounding pairs
+spec("user.extras.colorizer") -- color highlighter for "#xxxxxx" colors
 -- NOTE: Following are "development extras".
 spec("user.extras.gitsigns") -- show changes in sidebar
 spec("user.extras.neogit") -- "magit" like git interface
