b2d7d804c8177e88503da26f432180557232735f
dastagg:opus8 <dastagg@stagglinux.com>
2024-12-14 09:52:37 -0500

    Add illuminate.

--- init.lua
+++ init.lua
@@ -24,6 +24,7 @@ spec("user.extras.todo-comments") -- makes comments 'Light Up'
 spec("user.extras.navic-bread") -- provide context about code in winbar
 spec("user.extras.indent-blankline") -- provide indentation lines for code; integrated with rainbow-delimiters
 spec("user.extras.rainbow-delimiters") -- alternating syntax highlighting
+spec("user.extras.illuminate") -- highlight all other words under cursor
 -- NOTE: Following are "development extras".
 spec("user.extras.gitsigns") -- show changes in sidebar
 spec("user.extras.neogit") -- "magit" like git interface
