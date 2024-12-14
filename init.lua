5882845f4053d5934a4e4bd11393a88ef4d3b35d
dastagg:opus8 <dastagg@stagglinux.com>
2024-12-14 10:16:00 -0500

    Add autopairs and nvim-surround.

--- init.lua
+++ init.lua
@@ -25,7 +25,9 @@ spec("user.extras.navic-bread") -- provide context about code in winbar
 spec("user.extras.indent-blankline") -- provide indentation lines for code; integrated with rainbow-delimiters
 spec("user.extras.rainbow-delimiters") -- alternating syntax highlighting of {}, (), [] and more
 spec("user.extras.illuminate") -- highlight all other words under cursor
+spec("user.extras.autopairs") -- auto create the other half of the pair of {}, (), []
 spec("user.extras.neotab") -- tab in and out of {}, (), [] and more
+spec("user.extras.nvim-surround") -- Add/delete/change surrounding pairs
 -- NOTE: Following are "development extras".
 spec("user.extras.gitsigns") -- show changes in sidebar
 spec("user.extras.neogit") -- "magit" like git interface
