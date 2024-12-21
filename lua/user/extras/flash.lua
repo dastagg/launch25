59f74a4731172660f4915a871dc687c7a68a5a3f
dastagg:opus8 <dastagg@stagglinux.com>
2024-12-21 14:33:54 -0500

    Reverted to original code. New didn't work right.

--- lua/user/extras/flash.lua
+++ lua/user/extras/flash.lua
@@ -1,26 +1,17 @@
 local M = {
   "folke/flash.nvim",
   event = "VeryLazy",
+  ---@type Flash.Config
+  ---@diagnostic disable-next-line: missing-fields
+  opts = {},
+  -- stylua: ignore
+  keys = {
+    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+  },
 }
-
-function M.config()
-  local keymap = vim.keymap.set
-
-  keymap({ "n", "x", "o" }, "s", function()
-    require("flash").jump()
-  end, { noremap = true, silent = false, desc = "Flash" })
-  keymap({ "n", "x", "o" }, "S", function()
-    require("flash").treesitter()
-  end, { noremap = true, silent = false, desc = "Flash Treesitter" })
-  keymap("o", "r", function()
-    require("flash").remote()
-  end, { noremap = true, silent = false, desc = "Remote Flash" })
-  keymap({ "o", "x" }, "R", function()
-    require("flash").treesitter_search()
-  end, { noremap = true, silent = false, desc = "Treesitter Search" })
-  keymap("c", "<c-s>", function()
-    require("flash").toggle()
-  end, { noremap = true, silent = false, desc = "Toggle Flash Search" })
-end
 
 return M
